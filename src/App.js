cf810ceb8a89d270237f6ccf3a7046f0deeee16a
omaima <siafomaima5@gmail.com>
2024-12-25 12:33:05 +0000

    last save 2  before pushing by omaima

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './components/NavComp'
 import './App.css';
-import NavComp from './components/NavComp';
 function App() {
   return (
     <div className="App">
-      <NavComp/>
     </div>
   );
 }
